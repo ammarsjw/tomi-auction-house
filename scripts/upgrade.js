03f9a2dfaf2ce223a90addee265acdb838866d1c
ammarsjw <ammarsjw@gmail.com>
2023-04-06 21:55:20 +0500

    cc

--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -1,7 +1,7 @@
 const { ethers, upgrades } = require("hardhat")
 
 async function main() {
-  const newImplementation = await ethers.getContractFactory("contracts/legacy/LegacyAuctionHouse.sol:AuctionHouse")
+  const newImplementation = await ethers.getContractFactory("AuctionHouse")
   console.log("Upgrading AuctionHouse")
   await upgrades.upgradeProxy(
     "0x55B2b0a3e814C870eC60648E7a02860c2326Dd2A", // old proxy address
