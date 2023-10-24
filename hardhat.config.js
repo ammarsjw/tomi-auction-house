9e9732e64712e51c50d1696d9a53025a1e88bf02
ammarsjw <ammarsjw@gmail.com>
2023-10-24 18:12:11 +0500

    rm

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,30 +3,30 @@ require("@openzeppelin/hardhat-upgrades");
 require("dotenv").config();
 
 module.exports = {
-  solidity: {
-    compilers: [
-      {
-        version: '0.8.19',
-        settings: {
-          optimizer: {
-            enabled: false,
-            runs: 200,
-          },
-        },
-      },
-    ],
-  },
-  networks: {
-    goerli: {
-      url: process.env.URL,
-      accounts: [process.env.PRIVATE_KEY],
+    solidity: {
+        compilers: [
+            {
+                version: '0.8.19',
+                settings: {
+                    optimizer: {
+                        enabled: false,
+                        runs: 200,
+                    },
+                },
+            },
+        ],
     },
-  },
-  etherscan: {
-    apiKey: 'AYBZ53EN445WNPFP2IZ85RXRPB4FH5XBP7'
-  },
+    networks: {
+        goerli: {
+            url: process.env.URL_GOERLI,
+            accounts: [process.env.PRIVATE_KEY_GOERLI],
+        },
+        mainnet: {
+            url: process.env.URL_MAINNET,
+            accounts: [process.env.PRIVATE_KEY_MAINNET],
+        },
+    },
+    etherscan: {
+        apiKey: BLOCK_EXPLORER_API_KEY_ETHEREUM
+    },
 };
-
-
-// PXZCMGW77S5KIKPBINADIDBFFNCFMYVP6R  mumbaiScan-apikey
-// AYBZ53EN445WNPFP2IZ85RXRPB4FH5XBP7  etherscan-apikey
